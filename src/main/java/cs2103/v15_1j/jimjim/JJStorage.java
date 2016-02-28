c7e6e46a52d502470a59558e72cdfcc0a191d7bd
Shaun Thium <shaun.thium@gmail.com>
2016-02-28 18:47:21 +0800

    Create JJStorage.java skeleton

---
+++ src/main/java/cs2103/v15_1j/jimjim/JJStorage.java
@@ -0,0 +1,37 @@
+package cs2103.v15_1j.jimjim;
+
+import java.util.List;
+
+class JJStorage implements Storage {
+
+	@Override
+	public List<TaskEvent> getAll() {
+		
+		return null;
+	}
+
+	@Override
+	public boolean create(TaskEvent taskEvent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TaskEvent read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(int id, TaskEvent taskEvent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
