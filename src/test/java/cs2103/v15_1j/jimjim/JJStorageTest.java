a66711e20a6ae5e7609a80da3361b1cbdab2a84c
Shaun Thium <shaun.thium@gmail.com>
2016-02-28 22:26:16 +0800

    Create JUnit test for JJStorage

---
+++ src/test/java/cs2103/v15_1j/jimjim/JJStorageTest.java
@@ -0,0 +1,29 @@
+package cs2103.v15_1j.jimjim;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JJStorageTest {
+	private JJStorage storage;
+
+	@Before
+	public void setUp() throws Exception {
+		storage = new JJStorage("test.xml");
+	}
+	
+	@Test
+	public void testCreate() throws FileNotFoundException {
+		Task task1 = new Task("task1", LocalDateTime.now());
+		storage.create(task1);
+		
+		List<TaskEvent> list = storage.getAll();
+		assertEquals(1, list.size());
+	}
+
+}
