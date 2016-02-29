d528fdd9d95d72001a1341451d75635995759a52
Shaun Thium <shaun.thium@gmail.com>
2016-02-29 10:43:10 +0800

    Finish first implementation of Storage add() and getAll(). Unresolved bug with SignatureParser when testing.

--- src/main/java/cs2103/v15_1j/jimjim/JJStorage.java
+++ src/main/java/cs2103/v15_1j/jimjim/JJStorage.java
@@ -1,42 +1,71 @@
 package cs2103.v15_1j.jimjim;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.beans.XMLEncoder;
-import java.beans.XMLDecoder;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 class JJStorage implements Storage {
-	private static class JJStorageHelper {
-	    public static void write(List<TaskEvent> list, String filename) throws FileNotFoundException {
-	        XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
-	        encoder.writeObject(list);
-	        encoder.close();
-	    }
-
-	    public static List<TaskEvent> read(String filename) throws FileNotFoundException  {
-	        XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
-	        List<TaskEvent> list = (List<TaskEvent>) decoder.readObject();
-	        decoder.close();
-	        return list;
-	    }	
-	}
+//	private static class JJStorageHelper {
+//	    public static void write(List<TaskEvent> list, String filename) throws FileNotFoundException {
+//	        XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
+//	        encoder.writeObject(list);
+//	        encoder.close();
+//	    }
+//
+//	    public static List<TaskEvent> read(String filename) throws FileNotFoundException  {
+//	        XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
+//	        List<TaskEvent> list = (List<TaskEvent>) decoder.readObject();
+//	        decoder.close();
+//	        return list;
+//	    }	
+//	}
 	
 	private String fileName;
+	private File saveFile;
 	private List<TaskEvent> list;
+	
+	private void initializeList() {
+		try {
+//			JAXBContext context = new JAXBContext.newInstance(TaskEventWrapper.class);
+	        JAXBContext context = JAXBContext.newInstance(TaskEventWrapper.class);
+	        Unmarshaller um = context.createUnmarshaller();
+	        
+	        // Reading XML from the file and unmarshalling.
+	        TaskEventWrapper wrapper = (TaskEventWrapper) um.unmarshal(saveFile);
+	        list.clear();
+	        list.addAll(wrapper.getTaskEvents());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public JJStorage() throws FileNotFoundException  {
 		fileName = "data.xml";
-		list = JJStorageHelper.read(fileName); 
+		saveFile = new File(fileName);
+		initializeList();
+//		try {
+//			list = JJStorageHelper.read(fileName);
+//		} catch (FileNotFoundException e) {
+//			list = new ArrayList<TaskEvent>();
+//			JJStorageHelper.write(list, fileName);
+//		}
+	}
+	
+	
+	public JJStorage(String fileName) throws FileNotFoundException {
+		this.fileName = fileName;
+		saveFile = new File(fileName);
+		initializeList();
+//		try {
+//			list = JJStorageHelper.read(fileName);
+//		} catch (FileNotFoundException e) {
+//			list = new ArrayList<TaskEvent>();
+//			JJStorageHelper.write(list, fileName);
+//		}
 	}
 	
 //	private void retrieveList() throws IOException {
@@ -57,13 +86,27 @@ class JJStorage implements Storage {
 
 	@Override
 	public boolean create(TaskEvent taskEvent) {
-		try {
-			list.add(taskEvent);
-			JJStorageHelper.write(list, fileName);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
+		list.add(taskEvent);
+//			JJStorageHelper.write(list, fileName);
+		return saveListToFile();
+	}
+	
+	private boolean saveListToFile() {
+	    try {
+	        JAXBContext context = JAXBContext.newInstance(TaskEventWrapper.class);
+	        Marshaller m = context.createMarshaller();
+	        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+	        TaskEventWrapper wrapper = new TaskEventWrapper();
+	        wrapper.setTaskEvents(list);
+
+	        // Marshalling and saving XML to the file.
+	        m.marshal(wrapper, saveFile);
+	        
+	        return true;
+	    } catch (Exception e) { // catches ANY exception
+	    	return false;
+	    }
 	}
 
 	@Override
