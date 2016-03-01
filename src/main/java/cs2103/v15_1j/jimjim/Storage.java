afd9ed5904d4373d09566e04d39fcf91fbefe81f
Shaun Thium <shaun.thium@gmail.com>
2016-03-01 23:44:47 +0800

    Update Storage interface.

--- src/main/java/cs2103/v15_1j/jimjim/Storage.java
+++ src/main/java/cs2103/v15_1j/jimjim/Storage.java
@@ -2,9 +2,6 @@ package cs2103.v15_1j.jimjim;
 import java.util.List;
 
 public interface Storage {
-	public List<TaskEvent> getAll();
-	public boolean create(TaskEvent taskEvent);
-	public TaskEvent read(int id);
-	public boolean update(int id, TaskEvent taskEvent);
-	public boolean delete(int id);
+	public List<TaskEvent> load();
+	public void save(List<TaskEvent> list);
 }
