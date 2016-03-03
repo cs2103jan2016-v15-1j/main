ec623439a9b3a6b11308434041e5496f0f22921a
Shaun Thium <shaun.thium@gmail.com>
2016-03-03 23:33:45 +0800

    Complete JUnit JJStorageTest. Commented out AddTaskCommandTest.java

--- src/test/java/cs2103/v15_1j/jimjim/JJStorageTest.java
+++ src/test/java/cs2103/v15_1j/jimjim/JJStorageTest.java
@@ -2,10 +2,14 @@ package cs2103.v15_1j.jimjim;
 
 import static org.junit.Assert.*;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,16 +17,49 @@ public class JJStorageTest {
 	private JJStorage storage;
 
 	@Before
-	public void setUp() throws Exception {
-		storage = new JJStorage("test.xml");
+	public void setUp() {
+		storage = new JJStorage();
+		storage.setSaveFile("test.json");
 	}
 	
+	@After
+	public void tearDown() throws IOException {
+		// Delete test.json file after every test
+		storage.getSaveFile().delete();
+	}
+	
+	/*
+	 * Test that JJStorage is able to save file to disk
+	 */
 	@Test
-	public void testCreate() throws FileNotFoundException {
-		Task task1 = new Task("task1", LocalDateTime.now());
-		storage.create(task1);
+	public void testSave() throws IOException {
+		Task task = new Task("task", LocalDateTime.now());
+		List<TaskEvent> list = new ArrayList<TaskEvent>();
+		list.add(task);
 		
-		List<TaskEvent> list = storage.getAll();
-		assertEquals(1, list.size());
+		if (storage.save(list)) {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(storage.getSaveFile()));
+			assertNotNull(bufferedReader.readLine());
+			bufferedReader.close();
+		}
+	}
+	
+	/*
+	 * Test that JJStorage is able to successfully load data from disk
+	 */
+	@Test
+	public void testLoad() throws IOException {
+		Task task1 = new Task("task1", LocalDateTime.now());
+		Task task2 = new Task("task2", LocalDateTime.now());
+		List<TaskEvent> list = new ArrayList<TaskEvent>();
+		list.add(task1);
+		list.add(task2);
+		
+		if (storage.save(list)) {
+			List<TaskEvent> savedList = storage.loadFromDisk();
+			assertEquals(2, savedList.size());
+		} else {
+			fail("JJStorage was unable to save file.");
+		}
 	}
 }
