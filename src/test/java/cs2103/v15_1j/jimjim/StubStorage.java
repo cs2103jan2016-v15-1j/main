e4acc62f82c7e94da131289d444dcfe10174e02c
Tran Tien Dat <peter.trantiendat@gmail.com>
2016-03-07 17:47:41 +0800

    Implement logic for marking a task as done

---
+++ src/test/java/cs2103/v15_1j/jimjim/StubStorage.java
@@ -0,0 +1,31 @@
+package cs2103.v15_1j.jimjim;
+
+import java.util.List;
+
+public class StubStorage implements Storage {
+    
+    private boolean willCauseError = false;
+    
+    public void setStorageError() {
+        this.willCauseError = true;
+    }
+
+    @Override
+    public void setSaveFiles(String savedTasksFileName, String savedEventsFileName) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<TaskEvent> load() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean save(List<TaskEvent> list) {
+        return !willCauseError;
+    }
+
+    
+}
