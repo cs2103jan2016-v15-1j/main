72cb74bdeb4db3004096fd7432b47cc92a4e4130
Tran Tien Dat <peter.trantiendat@gmail.com>
2016-03-14 19:33:00 +0800

    Make displayList an observable list so that UI doesn't have to convert

--- src/main/java/cs2103/v15_1j/jimjim/Controller.java
+++ src/main/java/cs2103/v15_1j/jimjim/Controller.java
@@ -1,10 +1,10 @@
 package cs2103.v15_1j.jimjim;
-import java.util.List;
 import cs2103.v15_1j.jimjim.model.TaskEvent;
+import javafx.collections.ObservableList;
 
 public interface Controller {
 	public String execute(String userCommand);
-	public List<TaskEvent> getDisplayList();
+	public ObservableList<TaskEvent> getDisplayList();
 	public void setStorage(Storage storage);
 	public void setParser(Parser parser);
 	public void setSearcher(Searcher searcher);
--- src/main/java/cs2103/v15_1j/jimjim/JJController.java
+++ src/main/java/cs2103/v15_1j/jimjim/JJController.java
@@ -3,10 +3,12 @@ package cs2103.v15_1j.jimjim;
 import java.util.ArrayList;
 import java.util.List;
 import cs2103.v15_1j.jimjim.model.TaskEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class JJController implements Controller {
 	
-	private List<TaskEvent> displayList = new ArrayList<TaskEvent>();
+	private ObservableList<TaskEvent> displayList = FXCollections.observableArrayList();
 	private List<TaskEvent> list = new ArrayList<TaskEvent>();
 	private Parser parser;
 	private Searcher searcher;
@@ -19,7 +21,7 @@ public class JJController implements Controller {
 	}
 
 	@Override
-	public List<TaskEvent> getDisplayList() {
+	public ObservableList<TaskEvent> getDisplayList() {
 		return displayList;
 	}
 
