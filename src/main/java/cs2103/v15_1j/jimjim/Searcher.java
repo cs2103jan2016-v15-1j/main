396c856b0338eb834a0475995dbe433513d47766
Shaun Thium <shaun.thium@gmail.com>
2016-03-16 21:54:34 +0800

    Update Searcher interface with usage of DataLists and Filter.

--- src/main/java/cs2103/v15_1j/jimjim/Searcher.java
+++ src/main/java/cs2103/v15_1j/jimjim/Searcher.java
@@ -1,8 +1,8 @@
 package cs2103.v15_1j.jimjim;
+import java.util.ArrayList;
 import java.util.List;
 import cs2103.v15_1j.jimjim.model.TaskEvent;
 
 public interface Searcher {
-	public List<TaskEvent> search(String searchPhrase,
-								  List<TaskEvent> allTaskEvents);
+	public DataLists search(ArrayList<Filter> filters, DataLists masterLists);
 }
