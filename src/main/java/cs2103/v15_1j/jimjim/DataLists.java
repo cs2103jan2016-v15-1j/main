15bcf89668aef50f697f0aa1c1535de78393750f
Shaun Thium <shaun.thium@gmail.com>
2016-03-16 22:33:01 +0800

    Update DataLists interface.

--- src/main/java/cs2103/v15_1j/jimjim/DataLists.java
+++ src/main/java/cs2103/v15_1j/jimjim/DataLists.java
@@ -10,27 +10,23 @@ public class DataLists {
 	private ArrayList<FloatingTask> floatingTasksList;
 	private ArrayList<Event> eventsList;
 	
+	public DataLists(ArrayList<Task> tasksList,
+					 ArrayList<FloatingTask> floatingTasksList,
+					 ArrayList<Event> eventsList) {
+		this.tasksList = tasksList;
+		this.floatingTasksList = floatingTasksList;
+		this.eventsList = eventsList;
+	}
+	
 	public ArrayList<Task> getTasksList() {
 		return tasksList;
 	}
-	
-	public void setTasksList(ArrayList<Task> tasksList) {
-		this.tasksList = tasksList;
-	}
-	
+
 	public ArrayList<FloatingTask> getFloatingTasksList() {
 		return floatingTasksList;
 	}
-	
-	public void setFloatingTasksList(ArrayList<FloatingTask> floatingTasksList) {
-		this.floatingTasksList = floatingTasksList;
-	}
-	
+
 	public ArrayList<Event> getEventsList() {
 		return eventsList;
-	}
-	
-	public void setEventsList(ArrayList<Event> eventsList) {
-		this.eventsList = eventsList;
 	}
 }
