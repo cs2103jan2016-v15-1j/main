97930b2fed39f669aa90c6fbebf88cdbc05bdbc4
Shaun Thium <shaun.thium@gmail.com>
2016-03-17 08:40:26 +0800

    Update Searcher interface to use List instead of ArrayList.

--- src/main/java/cs2103/v15_1j/jimjim/Searcher.java
+++ src/main/java/cs2103/v15_1j/jimjim/Searcher.java
@@ -4,5 +4,5 @@ import java.util.List;
 import cs2103.v15_1j.jimjim.model.TaskEvent;
 
 public interface Searcher {
-	public DataLists search(ArrayList<Filter> filters, DataLists masterLists);
+	public DataLists search(List<Filter> filters, DataLists masterLists);
 }
