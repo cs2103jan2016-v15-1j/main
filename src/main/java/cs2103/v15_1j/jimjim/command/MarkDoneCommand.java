af178cc4924ab312cd76a58244c05b91c35863f0
Tran Tien Dat <peter.trantiendat@gmail.com>
2016-03-18 01:31:18 +0800

    Refactor temporary MarkDoneCommand

--- src/main/java/cs2103/v15_1j/jimjim/command/MarkDoneCommand.java
+++ src/main/java/cs2103/v15_1j/jimjim/command/MarkDoneCommand.java
@@ -1,10 +1,7 @@
 package cs2103.v15_1j.jimjim.command;
 
-import java.util.List;
-
 import cs2103.v15_1j.jimjim.DataLists;
 import cs2103.v15_1j.jimjim.model.DeadlineTask;
-import cs2103.v15_1j.jimjim.model.TaskEvent;
 import cs2103.v15_1j.jimjim.searcher.Searcher;
 import cs2103.v15_1j.jimjim.storage.Storage;
 
@@ -27,28 +24,24 @@ public class MarkDoneCommand implements Command {
 
     @Override
     public String execute(DataLists displayList, DataLists masterList, Storage storage, Searcher searcher) {
-        TaskEvent backup;
+        // TODO This only mark done for deadline tasks, not floating tasks
+        DeadlineTask task;
         try {
-            backup = displayList.remove(taskNum-1);
+            task = displayList.getDeadlineTasksList().remove(taskNum-1);
         } catch (IndexOutOfBoundsException e) {
             return "There is no item numbered " + this.taskNum;
         }
-        if (!(backup instanceof DeadlineTask)) {
-            displayList.add(taskNum-1, backup);
-            return "Number " + taskNum + " is an event, not a task!";
-        }
-        DeadlineTask task = (DeadlineTask) backup;
-        if (!masterList.contains(task)) {
+        if (!masterList.getDeadlineTasksList().contains(task)) {
             // synchronization issue between list and displayList
             // quietly add the task to list
-            masterList.add(task);
+            masterList.getDeadlineTasksList().add(task);
         }
         task.setCompleted(true);
         if (storage.save(masterList)) {
             return "Done!";
         } else {
             // failed to save, add the item back
-            displayList.add(taskNum-1, task);
+            displayList.getDeadlineTasksList().add(taskNum-1, task);
             task.setCompleted(false);
             return "Some error has occured. Please try again.";
         }
