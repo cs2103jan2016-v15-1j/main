3be29d8a6a5f6c1127289e0e585f44141e527a4c
Tran Tien Dat <peter.trantiendat@gmail.com>
2016-03-19 15:51:36 +0800

    Change delete to use the new way to identify task/event

--- src/main/java/cs2103/v15_1j/jimjim/DataLists.java
+++ src/main/java/cs2103/v15_1j/jimjim/DataLists.java
@@ -41,8 +41,25 @@ public class DataLists {
 	    } else if (taskEvent instanceof Event) {
 	        Event event = (Event) taskEvent;
 	        this.eventsList.add(event);
+	    } else {
+	        assert false;
 	    }
 	}
+
+    public void add(int i, TaskEvent taskEvent) {
+	    if (taskEvent instanceof DeadlineTask) {
+	        DeadlineTask deadlineTask = (DeadlineTask) taskEvent;
+	        this.deadlineTasksList.add(i, deadlineTask);
+	    } else if (taskEvent instanceof Task) {
+	        Task floatingTask = (Task) taskEvent;
+	        this.floatingTasksList.add(i, floatingTask);
+	    } else if (taskEvent instanceof Event) {
+	        Event event = (Event) taskEvent;
+	        this.eventsList.add(i, event);
+	    } else {
+	        assert false;
+	    }
+    }
 
 	public void remove(TaskEvent taskEvent) {
 	    if (taskEvent instanceof DeadlineTask) {
@@ -54,6 +71,25 @@ public class DataLists {
 	    } else if (taskEvent instanceof Event) {
 	        Event event = (Event) taskEvent;
 	        this.eventsList.remove(event);
+	    } else {
+	        assert false;
 	    }
 	}
+
+    public int indexOf(TaskEvent taskEvent) {
+	    if (taskEvent instanceof DeadlineTask) {
+	        DeadlineTask deadlineTask = (DeadlineTask) taskEvent;
+	        return this.deadlineTasksList.indexOf(deadlineTask);
+	    } else if (taskEvent instanceof Task) {
+	        Task floatingTask = (Task) taskEvent;
+	        return this.floatingTasksList.indexOf(floatingTask);
+	    } else if (taskEvent instanceof Event) {
+	        Event event = (Event) taskEvent;
+	        return this.eventsList.indexOf(event);
+	    } else {
+	        assert false;
+	        return 0;
+	    }
+    }
+
 }
