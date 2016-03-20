703abb2cef101ad44868c8402e73b9a33b30af08
Shaun Thium <shaun.thium@gmail.com>
2016-03-20 21:38:27 +0800

    Add parameter for Storage to set DeadlineTask file name.

--- src/main/java/cs2103/v15_1j/jimjim/storage/Storage.java
+++ src/main/java/cs2103/v15_1j/jimjim/storage/Storage.java
@@ -3,7 +3,7 @@ package cs2103.v15_1j.jimjim.storage;
 import cs2103.v15_1j.jimjim.DataLists;
 
 public interface Storage {
-	public void setSaveFiles(String savedTasksFileName, String savedEventsFileName);
+	public void setSaveFiles(String savedTasksFileName, String savedDeadlineTasksFileName, String savedEventsFileName);
 	public DataLists load();
 	public boolean save(DataLists list);
 }
