9bf40c118a5bbb96a35c993ab4b834963f1de9f2
Shaun Thium <shaun.thium@gmail.com>
2016-03-22 01:16:29 +0800

    Implement check() method for DateTimeFilter.

--- src/main/java/cs2103/v15_1j/jimjim/searcher/DateTimeFilter.java
+++ src/main/java/cs2103/v15_1j/jimjim/searcher/DateTimeFilter.java
@@ -1,7 +1,12 @@
 package cs2103.v15_1j.jimjim.searcher;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import cs2103.v15_1j.jimjim.model.DeadlineTask;
+import cs2103.v15_1j.jimjim.model.Event;
+import cs2103.v15_1j.jimjim.model.EventTime;
+import cs2103.v15_1j.jimjim.model.FloatingTask;
 import cs2103.v15_1j.jimjim.model.TaskEvent;
 
 public class DateTimeFilter implements Filter {
@@ -24,8 +29,26 @@ public class DateTimeFilter implements Filter {
 
     @Override
     public boolean check(TaskEvent taskEvent) {
-        // TODO Auto-generated method stub
-        return false;
+        if (taskEvent instanceof FloatingTask) {
+        	return false; // floating tasks have no datetime
+        } else if (taskEvent instanceof DeadlineTask) {
+        	DeadlineTask deadlineTask = (DeadlineTask) taskEvent;
+        	LocalDateTime deadline = deadlineTask.getDateTime();
+        	if ((start.compareTo(deadline) <= 0 ) && (end.compareTo(deadline) >= 0)) {
+        		return true;
+        	} else return false;
+        } else {
+        	Event event = (Event) taskEvent;
+        	List<EventTime> eventTimes = event.getDateTimes();
+        	for (EventTime eventTime : eventTimes) {
+        		LocalDateTime eventStart = eventTime.getStartDateTime();
+        		LocalDateTime eventEnd = eventTime.getEndDateTime();
+        		if ((start.compareTo(eventStart) <= 0) && (end.compareTo(eventEnd) >= 0)) {
+        			return true;
+        		}
+        	}
+        	return false;
+        }
     }
 
 }
