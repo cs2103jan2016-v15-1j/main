2b8db215533a4192d4220397889f58a5f3a19733
JeremyJDS <jeremyjds@me.com>
2016-03-25 18:34:27 +0800

    Added HelpFeedback Class

---
+++ src/main/java/cs2103/v15_1j/jimjim/uifeedback/HelpFeedback.java
@@ -0,0 +1,19 @@
+package cs2103.v15_1j.jimjim.uifeedback;
+
+import cs2103.v15_1j.jimjim.ui.MainViewController;
+
+public class HelpFeedback implements UIFeedback {
+
+	String message = "";
+	
+	public HelpFeedback(String message){
+		this.message = message;
+	}
+	
+	@Override
+	public String execute(MainViewController con) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
