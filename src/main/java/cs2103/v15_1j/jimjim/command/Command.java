2a38e70fda2cddabfc5ce25fa11c19156a5846c2
Shaun Thium <shaun.thium@gmail.com>
2016-03-26 12:35:12 +0800

    Update Command interface to include undoCommandHistory Stack.

--- src/main/java/cs2103/v15_1j/jimjim/command/Command.java
+++ src/main/java/cs2103/v15_1j/jimjim/command/Command.java
@@ -1,11 +1,13 @@
 package cs2103.v15_1j.jimjim.command;
+import java.util.Stack;
+
 import cs2103.v15_1j.jimjim.model.DataLists;
 import cs2103.v15_1j.jimjim.searcher.Searcher;
 import cs2103.v15_1j.jimjim.storage.Storage;
 
 public interface Command {
-	public String undo(DataLists displayList,
-					 DataLists masterList, Storage storage, Searcher searcher);
-	public String execute(DataLists displayList,
-						DataLists masterList, Storage storage, Searcher searcher);
+	public String undo(DataLists displayList, DataLists masterList, 
+					   Storage storage, Searcher searcher, Stack<Command> undoCommandHistory);
+	public String execute(DataLists displayList, DataLists masterList, 
+						  Storage storage, Searcher searcher, Stack<Command> undoCommandHistory);
 }
