5f03b4c2fadc8135b9f8fba97a04c4b928489ddf
Shaun Thium <shaun.thium@gmail.com>
2016-03-29 02:24:10 +0800

    Implement execute() method for ChangeCommand.

--- src/main/java/cs2103/v15_1j/jimjim/command/ChangeCommand.java
+++ src/main/java/cs2103/v15_1j/jimjim/command/ChangeCommand.java
@@ -1,31 +1,40 @@
 package cs2103.v15_1j.jimjim.command;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Stack;
 
 import cs2103.v15_1j.jimjim.model.DataLists;
+import cs2103.v15_1j.jimjim.model.DeadlineTask;
+import cs2103.v15_1j.jimjim.model.Event;
+import cs2103.v15_1j.jimjim.model.EventTime;
+import cs2103.v15_1j.jimjim.model.FloatingTask;
+import cs2103.v15_1j.jimjim.model.TaskEvent;
 import cs2103.v15_1j.jimjim.searcher.Searcher;
 import cs2103.v15_1j.jimjim.storage.Storage;
+import cs2103.v15_1j.jimjim.uifeedback.FailureFeedback;
 import cs2103.v15_1j.jimjim.uifeedback.UIFeedback;
 
-public class ChangeCommand implements Command {
+public class ChangeCommand implements UndoableCommand {
 
     private int taskNum;
     private char prefix;
     private String newName;
-    private LocalDate newDate;
-    private LocalTime newTime;
+    private LocalDate newStartDate;
+    private LocalTime newStartTime;
     private LocalDate newEndDate;
     private LocalTime newEndTime;
+    private TaskEvent backup;
     
     public ChangeCommand(char prefix, int num, String newName, LocalDate newDate,
             LocalTime newTime, LocalDate newEndDate, LocalTime newEndTime) {
         this.taskNum = num;
         this.prefix = prefix;
         this.newName = newName;
-        this.newDate = newDate;
-        this.newTime = newTime;
+        this.newStartDate = newDate;
+        this.newStartTime = newTime;
         this.newEndDate = newEndDate;
         this.newEndTime = newEndTime;
     }
@@ -43,11 +52,11 @@ public class ChangeCommand implements Command {
     }
     
     public LocalDate getNewDate() {
-        return newDate;
+        return newStartDate;
     }
     
     public LocalTime getNewTime() {
-        return newTime;
+        return newStartTime;
     }
     
     public LocalDate getNewEndDate() {
@@ -59,11 +68,61 @@ public class ChangeCommand implements Command {
     }
     
 	@Override
-	public UIFeedback execute(DataLists searchResultsList, DataLists masterList, 
-							  Storage storage, Searcher searcher, Stack<UndoableCommand> undoCommandHistory,
-							  Stack<UndoableCommand> redoCommandHistory) {
+	public UIFeedback undo(DataLists searchResultsList, DataLists masterList, Storage storage, Searcher searcher,
+			Stack<UndoableCommand> undoCommandHistory, Stack<UndoableCommand> redoCommandHistory) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	@Override
+	public UIFeedback execute(DataLists searchResultsList, DataLists masterList, 
+							  Storage storage, Searcher searcher, Stack<UndoableCommand> undoCommandHistory,
+							  Stack<UndoableCommand> redoCommandHistory) {
+		try {
+			backup = masterList.getTaskEvent(taskNum, prefix);
+            DeadlineTask tempDeadlineTask = null;
+            Event tempEvent = null;
+            if (backup instanceof DeadlineTask) {
+            	tempDeadlineTask = (DeadlineTask) backup;
+            } else if (backup instanceof Event) {
+            	tempEvent = (Event) backup;
+            }
+            
+            if (newName != null) {
+            	backup.setName(newName);
+            }
+            if (newStartDate != null) {
+            	if (backup instanceof DeadlineTask) {
+            		tempDeadlineTask.setDate(newStartDate);
+            	} else if (backup instanceof Event) {
+            		EventTime currentEventTime = tempEvent.getDateTimes().get(0);
+            		currentEventTime.setStartDate(newStartDate);
+            	}
+            }
+            if (newStartTime != null) {
+            	if (backup instanceof DeadlineTask) {
+            		tempDeadlineTask.setTime(newStartTime);
+            	} else if (backup instanceof Event) {
+            		EventTime currentEventTime = tempEvent.getDateTimes().get(0);
+            		currentEventTime.setStartTime(newStartTime);
+            	}
+            }
+            if (newEndDate != null) {
+            	if (backup instanceof Event) {
+            		EventTime currentEventTime = tempEvent.getDateTimes().get(0);
+            		currentEventTime.setEndDate(newEndDate);
+            	}
+            }
+            if (newEndTime != null) {
+            	if (backup instanceof Event) {
+            		EventTime currentEventTime = tempEvent.getDateTimes().get(0);
+            		currentEventTime.setEndTime(newEndTime);
+            	}
+            }
+		} catch (IndexOutOfBoundsException e) {
+			return new FailureFeedback(
+                    "There is no item numbered " + this.prefix + this.taskNum);
+		}
+		return null;
+	}
 }
