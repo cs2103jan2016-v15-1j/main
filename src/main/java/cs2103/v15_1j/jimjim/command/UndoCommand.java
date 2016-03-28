d3097b17180f28ff8dc0a7ef51b764092e27f126
Shaun Thium <shaun.thium@gmail.com>
2016-03-28 18:09:44 +0800

    Change UndoFeedback to FailureFeedback.

--- src/main/java/cs2103/v15_1j/jimjim/command/UndoCommand.java
+++ src/main/java/cs2103/v15_1j/jimjim/command/UndoCommand.java
@@ -5,15 +5,15 @@ import java.util.Stack;
 import cs2103.v15_1j.jimjim.model.DataLists;
 import cs2103.v15_1j.jimjim.searcher.Searcher;
 import cs2103.v15_1j.jimjim.storage.Storage;
+import cs2103.v15_1j.jimjim.uifeedback.FailureFeedback;
 import cs2103.v15_1j.jimjim.uifeedback.UIFeedback;
-import cs2103.v15_1j.jimjim.uifeedback.UndoFeedback;
 
 public class UndoCommand implements Command {
 	@Override
 	public UIFeedback execute(DataLists searchResultsList, DataLists masterList, 
 						  Storage storage, Searcher searcher, Stack<Command> undoCommandHistory) { 
 		if (undoCommandHistory.empty()) {
-			return new UndoFeedback("Nothing to undo!");
+			return new FailureFeedback("Nothing to undo!");
 		}
 		Command topCommand = undoCommandHistory.pop();
 		assert topCommand instanceof UndoableCommand;
