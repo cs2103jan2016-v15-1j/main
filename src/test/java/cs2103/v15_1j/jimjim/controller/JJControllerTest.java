d0d437114e13fe1d2900e34540cd370f57df06c0
Tran Tien Dat <peter.trantiendat@gmail.com>
2016-03-28 17:52:21 +0800

    Fix formatting of JJControllerTest

--- src/test/java/cs2103/v15_1j/jimjim/controller/JJControllerTest.java
+++ src/test/java/cs2103/v15_1j/jimjim/controller/JJControllerTest.java
@@ -24,35 +24,35 @@ import cs2103.v15_1j.jimjim.uifeedback.UIFeedback;
 
 public class JJControllerTest {
 
-	private final String DEADLINE_TASK_FILE_NAME = "deadline_tasks_test.json";
-	private final String TASK_FILE_NAME = "floating_tasks_test.json";
-	private final String EVENT_FILE_NAME = "events_test.json";
+    private final String DEADLINE_TASK_FILE_NAME = "deadline_tasks_test.json";
+    private final String TASK_FILE_NAME = "floating_tasks_test.json";
+    private final String EVENT_FILE_NAME = "events_test.json";
 
     private JJController controller;
     private JJStorage storage;
     private JJParser parser;
     private JJSearcher searcher;
-    
+
     @Before
     public void setUp() throws Exception {
         this.controller = new JJController();
-		this.storage = new JJStorage();
-		this.parser = new JJParser();
-		this.searcher = new JJSearcher();
+        this.storage = new JJStorage();
+        this.parser = new JJParser();
+        this.searcher = new JJSearcher();
 
-		storage.setSaveFiles(TASK_FILE_NAME, DEADLINE_TASK_FILE_NAME, EVENT_FILE_NAME);
-		controller.setParser(parser);
-		controller.setStorage(storage);
-		controller.setSearcher(searcher);
+        storage.setSaveFiles(TASK_FILE_NAME, DEADLINE_TASK_FILE_NAME, EVENT_FILE_NAME);
+        controller.setParser(parser);
+        controller.setStorage(storage);
+        controller.setSearcher(searcher);
     }
 
-	@After
-	public void tearDown() throws IOException {
-		// Delete test JSON files after every test
-		storage.getSavedFloatingTasksFile().delete();
-		storage.getSavedDeadlineTasksFile().delete();
-		storage.getSavedEventsFile().delete();
-	}
+    @After
+    public void tearDown() throws IOException {
+        // Delete test JSON files after every test
+        storage.getSavedFloatingTasksFile().delete();
+        storage.getSavedDeadlineTasksFile().delete();
+        storage.getSavedEventsFile().delete();
+    }
 
     @Test
     public void testEmptyInit() {
@@ -64,14 +64,13 @@ public class JJControllerTest {
         assertTrue(controller.getSearchResultsList().getFloatingTasksList().isEmpty());
         assertTrue(controller.getSearchResultsList().getEventsList().isEmpty());
     }
-    
+
     @Test
     public void testAdd() {
         UIFeedback feedback = controller.execute("Prepare for German exams");
         // it is added properly
         assertEquals(1, controller.getMasterList().getFloatingTasksList().size());
-        FloatingTask floatingTask = controller.getMasterList()
-                .getFloatingTasksList().get(0);
+        FloatingTask floatingTask = controller.getMasterList().getFloatingTasksList().get(0);
         assertEquals("Prepare for German exams", floatingTask.getName());
         assertFalse(floatingTask.getCompleted());
         // it returns the right feedback
@@ -81,19 +80,16 @@ public class JJControllerTest {
         // it is saved to disk
         DataLists listReadFromDisk = storage.load();
         assertEquals(1, listReadFromDisk.getFloatingTasksList().size());
-        floatingTask = listReadFromDisk
-                .getFloatingTasksList().get(0);
+        floatingTask = listReadFromDisk.getFloatingTasksList().get(0);
         assertEquals("Prepare for German exams", floatingTask.getName());
         assertFalse(floatingTask.getCompleted());
 
         feedback = controller.execute("Prepare for German exams by April 20th 2016");
         // it is added properly
         assertEquals(1, controller.getMasterList().getDeadlineTasksList().size());
-        DeadlineTask deadlineTask = controller.getMasterList()
-                .getDeadlineTasksList().get(0);
+        DeadlineTask deadlineTask = controller.getMasterList().getDeadlineTasksList().get(0);
         assertEquals("Prepare for German exams", floatingTask.getName());
-        assertEquals(LocalDateTime.of(LocalDate.of(2016, 4, 20), LocalTime.MAX),
-                deadlineTask.getDateTime());
+        assertEquals(LocalDateTime.of(LocalDate.of(2016, 4, 20), LocalTime.MAX), deadlineTask.getDateTime());
         assertFalse(deadlineTask.getCompleted());
         // it returns the right feedback
         assertTrue(feedback instanceof AddFeedback);
@@ -102,11 +98,9 @@ public class JJControllerTest {
         // it is saved to disk
         listReadFromDisk = storage.load();
         assertEquals(1, listReadFromDisk.getDeadlineTasksList().size());
-        deadlineTask = listReadFromDisk
-                .getDeadlineTasksList().get(0);
+        deadlineTask = listReadFromDisk.getDeadlineTasksList().get(0);
         assertEquals("Prepare for German exams", floatingTask.getName());
-        assertEquals(LocalDateTime.of(LocalDate.of(2016, 4, 20), LocalTime.MAX),
-                deadlineTask.getDateTime());
+        assertEquals(LocalDateTime.of(LocalDate.of(2016, 4, 20), LocalTime.MAX), deadlineTask.getDateTime());
         assertFalse(deadlineTask.getCompleted());
 
         feedback = controller.execute("German exams on 21/4/2016 10am to 11.30am");
@@ -114,10 +108,8 @@ public class JJControllerTest {
         assertEquals(1, controller.getMasterList().getEventsList().size());
         Event event = controller.getMasterList().getEventsList().get(0);
         assertEquals("German exams", event.getName());
-        assertEquals(LocalDateTime.of(2016, 4, 21, 10, 00),
-                event.getDateTimes().get(0).getStartDateTime());
-        assertEquals(LocalDateTime.of(2016, 4, 21, 11, 30),
-                event.getDateTimes().get(0).getEndDateTime());
+        assertEquals(LocalDateTime.of(2016, 4, 21, 10, 00), event.getDateTimes().get(0).getStartDateTime());
+        assertEquals(LocalDateTime.of(2016, 4, 21, 11, 30), event.getDateTimes().get(0).getEndDateTime());
         // it returns the right feedback
         assertTrue(feedback instanceof AddFeedback);
         addFeedback = (AddFeedback) feedback;
@@ -125,15 +117,12 @@ public class JJControllerTest {
         // it is saved to disk
         listReadFromDisk = storage.load();
         assertEquals(1, listReadFromDisk.getEventsList().size());
-        event = listReadFromDisk
-                .getEventsList().get(0);
+        event = listReadFromDisk.getEventsList().get(0);
         assertEquals("German exams", event.getName());
-        assertEquals(LocalDateTime.of(2016, 4, 21, 10, 00),
-                event.getDateTimes().get(0).getStartDateTime());
-        assertEquals(LocalDateTime.of(2016, 4, 21, 11, 30),
-                event.getDateTimes().get(0).getEndDateTime());
+        assertEquals(LocalDateTime.of(2016, 4, 21, 10, 00), event.getDateTimes().get(0).getStartDateTime());
+        assertEquals(LocalDateTime.of(2016, 4, 21, 11, 30), event.getDateTimes().get(0).getEndDateTime());
     }
-    
+
     @Test
     public void testDelete() {
         controller.execute("Prepare for German exams");
