51b9982bfe9308b8e6975cab302bf8e28a1dbf84
JeremyJDS <jeremyjds@me.com>
2016-04-01 21:46:40 +0800

    Remove EventTime

--- src/main/java/cs2103/v15_1j/jimjim/model/Event.java
+++ src/main/java/cs2103/v15_1j/jimjim/model/Event.java
@@ -1,5 +1,8 @@
 package cs2103.v15_1j.jimjim.model;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,50 +10,98 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class Event extends TaskEvent implements Comparable<Event> {
-	private ObjectProperty<List<EventTime>> dateTimes;
+	private ObjectProperty<LocalDateTime> startDateTime;
+	private ObjectProperty<LocalDateTime> endDateTime;
 
 	public Event(String name, LocalDateTime start, LocalDateTime end) {
 		super(name);
-		this.dateTimes = new SimpleObjectProperty<List<EventTime>>(new ArrayList<EventTime>());
-		this.dateTimes.get().add(new EventTime(start, end));
+		this.setStartDateTime(start);
+		this.setEndDateTime(end);
 	}
 	
-	public Event(Event other) {
-		this(other.getName(), other.getEarliestDateTime(), other.getLatestDateTime());
+	public Event(Event e){
+		this(e.getName(), e.getStartDateTime(), e.getEndDateTime());
 	}
 
-	public List<EventTime> getDateTimes() {
-		return this.dateTimes.get();
+	public LocalDateTime getStartDateTime() {
+		return startDateTime.get();
 	}
 
-	public void addDateTime(LocalDateTime start, LocalDateTime end){
-		dateTimes.get().add(new EventTime(start, end));
-	}
-
-	public ObjectProperty<List<EventTime>> dateTimesProperty(){
-		return dateTimes;
+	public void setStartDate(LocalDate startDate){
+		LocalDateTime temp = LocalDateTime.of(startDate, startDateTime.get().toLocalTime());
+		this.startDateTime = new SimpleObjectProperty<LocalDateTime>(temp);
 	}
 	
-	public LocalDateTime getEarliestDateTime(){
-		return dateTimes.get().get(0).getStartDateTime();
+	public void setStartTime(LocalTime startTime) {
+		LocalDateTime temp = LocalDateTime.of(startDateTime.get().toLocalDate(), startTime);
+		this.startDateTime = new SimpleObjectProperty<LocalDateTime>(temp);
+	}
+
+	public void setStartDateTime(LocalDateTime startDateTime) {
+		this.startDateTime = new SimpleObjectProperty<LocalDateTime>(startDateTime);
+	}
+
+	public ObjectProperty<LocalDateTime> startDateTimeProperty(){
+		return startDateTime;
+	}
+
+	public ObjectProperty<LocalDate> startDateProperty(){
+		return new SimpleObjectProperty<LocalDate>(startDateTime.get().toLocalDate());
+	}
+
+	public LocalDateTime getEndDateTime() {
+		return endDateTime.get();
+	}
+
+	public void setEndDate(LocalDate endDate){
+		LocalDateTime temp = LocalDateTime.of(endDate, startDateTime.get().toLocalTime());
+		this.endDateTime = new SimpleObjectProperty<LocalDateTime>(temp);
 	}
 	
-	public LocalDateTime getLatestDateTime(){
-		return dateTimes.get().get(dateTimes.get().size() - 1).getEndDateTime();
+	public void setEndTime(LocalTime endTime){
+		LocalDateTime temp = LocalDateTime.of(startDateTime.get().toLocalDate(), endTime);
+		this.endDateTime = new SimpleObjectProperty<LocalDateTime>(temp);
+	}
+
+	public void setEndDateTime(LocalDateTime endDateTime) {
+		this.endDateTime = new SimpleObjectProperty<LocalDateTime>(endDateTime);
+	}
+
+	public ObjectProperty<LocalDateTime> endDateTimeProperty(){
+		return startDateTime;
+	}
+
+	public ObjectProperty<LocalDate> endDateProperty(){
+		return new SimpleObjectProperty<LocalDate>(endDateTime.get().toLocalDate());
+	}
+
+	public String toDateTimeString(){
+		DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("dd MMM h:mm a");
+		DateTimeFormatter timeFmt = DateTimeFormatter.ofPattern("h:mm a");
+
+		if(startDateTime.get().toLocalDate().equals(endDateTime.get().toLocalDate())){
+			return startDateTime.get().format(dateFmt) + " - " + endDateTime.get().format(timeFmt);
+		}
+
+		return startDateTime.get().format(dateFmt) + " - " + endDateTime.get().format(dateFmt);
+	}
+
+	public String toTimeString(){
+		DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("h:mm a");
+
+		return startDateTime.get().format(dateFmt) + " - " + endDateTime.get().format(dateFmt);
 	}
 
 	@Override
 	public int compareTo(Event o) {
-		LocalDateTime firstStartDateTime = dateTimes.get().get(0).getStartDateTime();
-		LocalDateTime otherStartDateTime = o.getDateTimes().get(0).getStartDateTime();
 
-		if(firstStartDateTime.compareTo(otherStartDateTime) == 0){
+		if(startDateTime.get().compareTo(o.startDateTime.get()) == 0){
 			String eventName = this.getName().toLowerCase();
 			String otherName = o.getName().toLowerCase();
 
 			return eventName.compareTo(otherName);
 		} else {
-			return firstStartDateTime.compareTo(otherStartDateTime);
+			return startDateTime.get().compareTo(o.startDateTime.get());
 		}
 	}
 	
@@ -60,7 +111,7 @@ public class Event extends TaskEvent implements Comparable<Event> {
 			return false;
 		}
 		Event other = (Event) t;
-		return this.getName().equals(other.getName())
-			&& this.getDateTimes().equals(other.getDateTimes());
+		return this.getName().equals(other.getName()) && this.getStartDateTime().equals(other.getStartDateTime()) &&
+			   this.getEndDateTime().equals(other.getEndDateTime());
 	}
 }
