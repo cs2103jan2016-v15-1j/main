1aa33248ebc8142a854c79210b16449360608444
Shaun Thium <shaun.thium@gmail.com>
2016-04-03 01:24:57 +0800

    Implement execute() method for AliasAddCommand.

--- src/main/java/cs2103/v15_1j/jimjim/command/AliasAddCommand.java
+++ src/main/java/cs2103/v15_1j/jimjim/command/AliasAddCommand.java
@@ -1,6 +1,10 @@
 package cs2103.v15_1j.jimjim.command;
 
 import cs2103.v15_1j.jimjim.controller.ControllerStates;
+import cs2103.v15_1j.jimjim.uifeedback.AliasAddFeedback;
+import cs2103.v15_1j.jimjim.uifeedback.AliasDeleteFeedback;
+import cs2103.v15_1j.jimjim.uifeedback.DeleteFeedback;
+import cs2103.v15_1j.jimjim.uifeedback.FailureFeedback;
 import cs2103.v15_1j.jimjim.uifeedback.UIFeedback;
 
 public class AliasAddCommand implements UndoableCommand {
@@ -29,16 +33,35 @@ public class AliasAddCommand implements UndoableCommand {
     }
 
     @Override
-    public UIFeedback execute(ControllerStates conStates) {
-        // just need to update conStates.config.aliases
-        // TODO Auto-generated method stub
-        return null;
+    public UIFeedback undo(ControllerStates conStates) {
+    	conStates.config.aliases.remove(alias, keyword);
+    	
+	    if (conStates.storage.saveConfig(conStates.config)) {
+	    	conStates.redoCommandHistory.push(this);
+	        return new AliasDeleteFeedback(keywordString);
+	    } else {
+	        // If conStates.storage fails to save list
+	        // add alias back to config
+	        conStates.config.aliases.put(alias, keyword);
+	        conStates.undoCommandHistory.push(this);
+	        return new FailureFeedback("Some error has occured. Please try again.");
+	    }
     }
 
     @Override
-    public UIFeedback undo(ControllerStates conStates) {
-        // TODO Auto-generated method stub
-        return null;
+    public UIFeedback execute(ControllerStates conStates) {
+        // just need to update conStates.config.aliases
+    	conStates.config.aliases.put(alias, keyword);
+    	
+        if (conStates.storage.saveConfig(conStates.config)) {
+        	conStates.undoCommandHistory.push(this);
+        	return new AliasAddFeedback(keywordString);
+        } else {
+            // If conStates.storage fails to save config
+            // remove alias
+        	conStates.redoCommandHistory.push(this);
+            conStates.config.aliases.remove(alias, keyword);
+            return new FailureFeedback("Some error has occured. Please try again.");
+        }
     }
-
 }
