178752bb7701fb5fb5f245320963723f31298a45
Shaun Thium <shaun.thium@gmail.com>
2016-04-04 18:33:43 +0800

    Add checking for invalid file paths/extensions.

--- src/main/java/cs2103/v15_1j/jimjim/command/SaveLocationCommand.java
+++ src/main/java/cs2103/v15_1j/jimjim/command/SaveLocationCommand.java
@@ -1,5 +1,10 @@
 package cs2103.v15_1j.jimjim.command;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
 import cs2103.v15_1j.jimjim.controller.ControllerStates;
 import cs2103.v15_1j.jimjim.uifeedback.FailureFeedback;
 import cs2103.v15_1j.jimjim.uifeedback.SaveLocationFeedback;
@@ -20,10 +25,22 @@ public class SaveLocationCommand implements Command {
     @Override
     public UIFeedback execute(ControllerStates conStates) {
     	String backup = conStates.config.savePath;
+    	
+    	try {
+    		File f = new File(backup);	
+    	} catch (Exception e) {
+    		return new FailureFeedback("Invalid file path specified!");
+    	}
+    	
+    	String extension = savePath.substring(savePath.length()-5, savePath.length()); 
+    	if (!extension.equals(".json")) {
+    		return new FailureFeedback("Wrong file extension specified!");
+    	}
+    	
     	conStates.config.savePath = savePath;
+    	conStates.storage.setSaveFile(savePath);
     	
     	if (conStates.storage.saveConfig(conStates.config)) {
-    		conStates.storage.setSaveFile(savePath);
         	return new SaveLocationFeedback(savePath);
         } else {
             // If conStates.storage fails to save config
